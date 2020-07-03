45a202051104d860c3d75aa7419a95034900ae5f
KamathAS <aiswarya.kamath@in.unisys.com>
2020-07-03 22:05:37 +0530

    butger builder- adding state to burger ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import classes from './Burger.css'
-import BurgerIngedient from './BurgerIngredient/BurgerIngredient'
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngedient from "./BurgerIngredient/BurgerIngredient";
+import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 //import BurgerIngedient from './BurgerIngredient/BurgerIngredient'
 
-
-const burger = (props) =>{
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngedient type="bread-top"/>
-            <BurgerIngedient type="cheese"/>
-            <BurgerIngedient type="meat"/>
-            <BurgerIngedient type="bread-bottom"/>
-        </div>
-    );
-}
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        console.log(props.ingredients);
+        return [...Array(props.ingredients[igKey])].map(( _, i) => {
+            return <BurgerIngedient key={igKey + i} type={igKey} />;
+        });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngedient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngedient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+    
+    state = {
+        ingredient : {
+            salad:4,
+            bacon:2,
+            cheese:3,
+            meat:1
+        }
+    }
+
     render() {
         return ( 
         <Auxiliary>
-            <Burger/>
+            <Burger ingredients={this.state.ingredient}/>
             <div>Burger Controls</div>
         </Auxiliary>
 
