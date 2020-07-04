24e17c18ec1b2e6bd898f9116cc52b084c83c514
KamathAS <aiswarya.kamath@in.unisys.com>
2020-07-04 23:26:43 +0530

    Adding and removing components from burger logic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,25 @@
 import React from "react";
 import classes from "./Burger.css";
 import BurgerIngedient from "./BurgerIngredient/BurgerIngredient";
-import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
+//import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 //import BurgerIngedient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-        console.log(props.ingredients);
+        //console.log(props.ingredients);
         return [...Array(props.ingredients[igKey])].map(( _, i) => {
             return <BurgerIngedient key={igKey + i} type={igKey} />;
         });
-  });
+  })
+  .reduce((curr,el)=>{
+      return curr.concat(el);
+  },[]);
+  console.log(transformedIngredients);
+
+  if(transformedIngredients.length===0){
+      transformedIngredients = <p> Please add ingredients!!</p>
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngedient type="bread-top" />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,27 +1,83 @@
-import React, {Component} from 'react';
-import Auxiliary from '../../hoc/Auxiliary';
-import Burger from '../../components/Burger/Burger'
+import React, { Component } from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
-class BurgerBuilder extends Component{
-    
-    state = {
-        ingredient : {
-            salad:4,
-            bacon:2,
-            cheese:3,
-            meat:1
-        }
+const INGEDIENT_PRICE = {
+  salad: 0.2,
+  bacon: 0.5,
+  meat: 1.2,
+  cheese: 1,
+};
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredient: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 4,
+  };
+
+  addIngredientHandler = (type) => {
+    //set new ingredient count
+    const oldCount = this.state.ingredient[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredient = {
+      ...this.state.ingredient,
+    };
+    updatedIngredient[type] = updatedCount;
+
+    //set new price
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + INGEDIENT_PRICE[type];
+
+    this.setState({
+      ingredient: updatedIngredient,
+      totalPrice: newPrice,
+    });
+  };
+
+  removeIngredientHandler = (type) => {
+    //set new ingredient count
+    const oldCount = this.state.ingredient[type];
+    // const updatedCount = --oldCount > 0 ? oldCount : 0;
+    if (oldCount === 0) return;
+    const updatedCount = oldCount - 1;
+    const updatedIngredient = {
+      ...this.state.ingredient,
+    };
+    updatedIngredient[type] = updatedCount;
+
+    //set new price
+    const oldPrice = this.state.totalPrice;
+    let newPrice = oldPrice - INGEDIENT_PRICE[type];
+    //newPrice = newPrice >= 4 ? newPrice : 4;
+    this.setState({
+      ingredient: updatedIngredient,
+      totalPrice: newPrice,
+    });
+  };
+
+  render() {
+    const disabledIngredient = { ...this.state.ingredient }; //object literal spread operator
+    for (let key in disabledIngredient) {
+      disabledIngredient[key] = disabledIngredient[key] <= 0;
     }
-
-    render() {
-        return ( 
-        <Auxiliary>
-            <Burger ingredients={this.state.ingredient}/>
-            <div>Burger Controls</div>
-        </Auxiliary>
-
-    )}
+    console.log(disabledIngredient);
+    return (
+      <Auxiliary>
+        <Burger ingredients={this.state.ingredient} />
+        <BuildControls
+          adIngredient={this.addIngredientHandler}
+          rmIngredient={this.removeIngredientHandler}
+          disabledIng={disabledIngredient}
+        />
+      </Auxiliary>
+    );
+  }
 }
-
 
 export default BurgerBuilder;
